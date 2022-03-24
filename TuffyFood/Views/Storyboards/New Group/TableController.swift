f4edbec8e8f6c5cd53aabda0bcd0b50f2ebc08bc
Nicholas Chung <70074654+Minsuhk@users.noreply.github.com>
2022-03-24 15:50:06 -0700

    checkin3

---
+++ TuffyFood/Views/Storyboards/New Group/TableController.swift
@@ -0,0 +1,20 @@
+//
+//  TableControler.swift
+//  TuffyFood
+//
+//  Created by csuftitan on 3/23/22.
+//
+
+import SwiftUI
+
+struct TableControler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TableControler_Previews: PreviewProvider {
+    static var previews: some View {
+        TableControler()
+    }
+}
